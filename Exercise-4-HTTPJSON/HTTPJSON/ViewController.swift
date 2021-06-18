eaed5302667a88625ca4d97bea3d2d8f0592539f
Georgina1998 <45017133+Georgina1998@users.noreply.github.com>
2021-06-17 23:38:45 -0500

    Initial commit

---
+++ Exercise-4-HTTPJSON/HTTPJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HTTPJSON
+//
+//  Created by Xavier Dash on 5/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Exercise-6-bgTasks/bgTasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bgTasks
+//
+//  Created by Andy Ibanez on 12/6/19.
+//  Copyright © 2019 BNB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
